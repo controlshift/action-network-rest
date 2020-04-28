f7b6b1ba07175efcf1c4006ca9cb1ab94d177d60
Grey Moore <grey@controlshiftlabs.com>
2020-04-28 15:53:43 -0400

    explanatory comment

--- lib/action_network_rest/base.rb
+++ lib/action_network_rest/base.rb
@@ -14,6 +14,16 @@ module ActionNetworkRest
     def object_from_response(response)
       obj = response.body
 
+      # The response we get from Action Network may contain an "identifiers" block that looks something like:
+      #
+      # "identifiers": [
+      #   "action_network:d6bdf50e-c3a4-4981-a948-3d8c086066d7",
+      #   "some_external_system:1",
+      #   "another_external_system:57"
+      # ]
+      #
+      # If so, we pull out the action_network identifier and stick it in a top-level key "action_network_id",
+      # for the convenience of callers using the returned object.
       identifiers = obj[:identifiers] || []
       qualified_actionnetwork_id = identifiers.find do |id|
         id.split(':').first == 'action_network'
