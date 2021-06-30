26fa34da5feee1ed42bd698d7ec142345367e9ec
Grey Moore <grey@controlshiftlabs.com>
2021-06-30 16:39:16 -0400

    basic spec for events.create

--- spec/events_spec.rb
+++ spec/events_spec.rb
@@ -45,4 +45,36 @@ describe ActionNetworkRest::Events do
       expect(stub_request).to have_been_requested
     end
   end
+
+  describe '#create' do
+    let(:event_data) do
+      {
+        identifiers: ['somesystem:123'],
+        title: 'My Great Event',
+        origin_system: 'Some System'
+      }
+    end
+    let(:request_body) { event_data }
+    let(:response_body) do
+      {
+        identifiers: ['somesystem:123', 'action_network:123-456-789-abc'],
+        title: 'My Great Event',
+        origin_system: 'Some System'
+      }.to_json
+    end
+    let!(:post_stub) do
+      stub_actionnetwork_request('/events/', method: :post, body: request_body)
+        .to_return(status: 200, body: response_body)
+    end
+
+    it 'should POST event data' do
+      event = subject.events.create(event_data)
+
+      expect(post_stub).to have_been_requested
+
+      expect(event.identifiers).to contain_exactly('action_network:123-456-789-abc',
+                                                   'somesystem:123')
+      expect(event.action_network_id).to eq '123-456-789-abc'
+    end
+  end
 end
