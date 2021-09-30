08d0cd8e4812f13c13702132ad92294645ba7d60
anero <anero79@gmail.com>
2021-09-30 11:58:42 -0300

    Raise error if attendance fails to be created

--- lib/action_network_rest/attendances.rb
+++ lib/action_network_rest/attendances.rb
@@ -2,6 +2,8 @@
 
 module ActionNetworkRest
   class Attendances < Base
+    class CreateError < StandardError; end
+
     attr_accessor :event_campaign_id, :event_id
 
     def base_path
@@ -14,7 +16,15 @@ module ActionNetworkRest
 
     def create(attendance_data)
       response = client.post_request base_path, attendance_data
-      object_from_response(response)
+      new_attendance = object_from_response(response)
+
+      # Action Network treats the attendance create helper endpoint as an unauthenticated
+      # "blind" POST (see https://actionnetwork.org/docs/v2/unauthenticated-post/). For this
+      # reason they don't return a status code with error to avoid leaking private data. Instead
+      # they return 200 OK with an empty body (vs. the newly created attendance's data for successful calls)
+      raise CreateError if new_attendance.empty?
+
+      new_attendance
     end
 
     def update(id, attendance_data)
